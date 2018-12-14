9a9dfffd17ab9693e14323df5161fbbf8e632ea8
Jitan Gupta <gtangupta@gmail.com>
2018-12-14 23:42:08 +0530

    demo changes

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -11,14 +11,16 @@ calendar.Create();
 
 
 //Function for reading JSON file
-function loadJSON(callback) {
+function loadJSON(callback) {   
+
   var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open("GET", "demo/demo-data.json", false);
-  xobj.onreadystatechange = function() {
-    if (xobj.readyState === 4 && xobj.status === "200") {
-      callback(xobj.responseText);
-    }
+      xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'demo/demo-data.json', false); // Replace 'my_data' with the path to your file
+  xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+          callback(xobj.responseText);
+        }
   };
-  xobj.send(null);
+  xobj.send(null);  
 }
